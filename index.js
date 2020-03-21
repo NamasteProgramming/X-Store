4fa0f436cfcea12e55576e876bb744c5ae90e85c
Namaste Programming <namasteprogramming@gmail.com>
2020-03-22 02:26:51 +0530

    Day 2 - Automatic refresh added using nodemon

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
+const app= express()
 
 app.get('/', function(req, res) {
-    return res.send('Hello World!');
+   return res.send('Hello, World!');
 })
 
 app.listen(3000, function() {
-    console.log('Server running at port 3000');
+                console.log('Server running at port 3000');
     
 })
