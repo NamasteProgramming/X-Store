7067c0021ad108015883e2fbae67a4937d18a00f
Namaste Programming <namasteprogramming@gmail.com>
2020-03-22 15:05:27 +0530

    Day 7 - Form handling added on register page

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const User = require('./models/User')
+
 require('./utils/db.config')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   return res.render('index')
+})
+
+app.get('/register', (req, res) => {
+  return res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+  const user = new User(req.body)
+  await user.save()
+  return res.render('register', { message: 'Registration successful' })
 })
 
 app.listen(3000, () => {
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [2, 'Name can\'t be smaller than 2 characters'],
+    maxlength: [64, 'Name can\'t be greater than 64 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    maxlength: [128, 'Email can\'t be greater than 128 characters'],
+    index: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
