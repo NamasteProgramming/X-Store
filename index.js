2537eeaa4424320a3056c932dbffb9003751e0c8
Namaste Programming <namasteprogramming@gmail.com>
2020-04-03 02:02:21 +0530

    Day 14 - Sessions added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 
@@ -7,10 +8,19 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
+// app.set('trust proxy', 1)
+app.use(session({
+  secret: '788a154e2a8d07c4cafdee4a7d6dff2d90ab2586',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`You have visited ${req.session.views} times`)
   return res.render('index')
 })
 
