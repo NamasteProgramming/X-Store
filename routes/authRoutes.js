9eeee5a2fcd8b62d79aeab132df8dff20372a840
Namaste Programming <namasteprogramming@gmail.com>
2020-04-16 20:16:06 +0530

    Day 15 - Create a login page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -51,4 +51,25 @@ router.post('/register', async (req, res) => {
   }
 })
 
+/**
+ * Shows page for user login
+ */
+router.get('/login', (req, res) => {
+  return res.render('login', { message: {}, formData: {}, errors: {} })
+})
+
+/**
+ * Logs in a user
+ */
+router.post('/login', (req, res) => {
+  return res.render('login', {
+    message: {
+      type: 'success',
+      body: 'Login success'
+    },
+    formData: {},
+    errors: {}
+  })
+})
+
 module.exports = router
