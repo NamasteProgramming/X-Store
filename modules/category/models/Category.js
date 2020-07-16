af7308571eb9b50f8d1edb18867b5284c7b3e11c
Namaste Programming <namasteprogramming@gmail.com>
2020-07-17 00:32:36 +0530

    Day 33 - Finishing category UI

--- modules/category/models/Category.js
+++ modules/category/models/Category.js
@@ -6,11 +6,24 @@ const choicesSchema = {
     required: true
   },
   value: {
-    type: mongoose.Schema.Types.Mixed
+    type: mongoose.Schema.Types.Mixed,
+    required: true
   }
 }
 
 const categorySchema = mongoose.Schema({
+  /**
+   * OPTIONAL: Parent category id
+   * Ex: Electronics > Mobile
+   * In case of mobile it will store id of electronics category
+   */
+  categoryId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Category',
+    index:  true,
+    sparse: true // Required with optional unique index
+  },
+
   /**
    * Name of the category
    */
@@ -21,6 +34,22 @@ const categorySchema = mongoose.Schema({
     maxlength: [32, 'Name can\'t be greater than 64 characters']
   },
 
+  /**
+   * Is category a leaf category
+   * Leaf category have product as child
+   * Non leaf category have category as child
+   * Ex: Electronics > Mobile
+   * Here electronics is not leaf category because it doesn't have any product
+   * Mobile is leaf category because it has products as child
+   */
+  isLeaf: {
+    type: Boolean,
+    required: true
+  },
+
+  /**
+   * TEXT ONLY: Category description
+   */
   description: {
     type: String,
     required: [true, 'Description is required']
@@ -34,16 +63,27 @@ const categorySchema = mongoose.Schema({
     max: 160
   },
 
+  /**
+   * Category thumbnail
+   */
   thumbnail: {
-    type: String
+    path: String, // Path is equivalent to URL
+    driver: String // Driver refers to local, s3, ftp, etc.
   },
 
   /**
-   * Filters that applies to this category
+   * OPTIONAL: Properties that applies to this category
    */
   properties: [{
     /**
-     * Filter name like RAM, Storage
+     * Is it required on product
+     */
+    required: {
+      type: Boolean
+    },
+
+    /**
+     * Property name like RAM, Storage
      */
     name: {
       type: String,
@@ -51,17 +91,18 @@ const categorySchema = mongoose.Schema({
     },
 
     /**
+     * Only filterable properties will show in the list
+     */
+    filterable: {
+      type: Boolean,
+      default: false
+    },
+
+    /**
      * Optional
      * Possible choices for filter like 1GB, 2GB
      */
-    filters: [choicesSchema],
-
-    /**
-     * Is it required on product
-     */
-    required: {
-      type: Boolean
-    },
+    filterChoices: [choicesSchema],
 
     /**
      * Input Type
@@ -73,14 +114,36 @@ const categorySchema = mongoose.Schema({
         /**
          * What kind of input is shown to product detail input
          */
-        enum: ['fractionalNumberInput', 'completeNumberInput', 'textInput', 'selectOne', 'selectMultiple']
+        enum: [
+          'fractionalNumber', // Numbers with decimal points
+          'completeNumber', // Numbers without decimal points
+          'textOneline', // One line of text
+          'textMultiline', // Paragraph line of text
+          'selectOne', // Dropdown with abitlity to select one
+          'selectMultiple' // Dropdown with abiltiy to select multiple
+        ]
       },
 
       /**
-       * Available values for filter
+       * When input type is select, this array is used to generate dropdown
        */
       propertyChoices: [choicesSchema]
-    }
+    },
+
+    /**
+     * Applied unit to this category
+     * In case of storage it can be MB, GB, TB, etc
+     * [{ label: "KB", threshold: 1024, nextLabel: "MB"},
+     * { label: "MB", threshold: 1024, nextLabel: "GB"},
+     * { label: "GB", threshold: 1024, nextLabel: "TB"},
+     * { label: "TB", threshold: 1024, nextLabel: "PB"}]
+     */
+    units: [{
+      label: String, // GB,
+      printLabel: String, // This will be printed on invoice
+      threshold: Number, // 1024
+      nextLabel: String
+    }]
   }],
 
   /**
--- public/js/admin/category_create.js
+++ public/js/admin/category_create.js
@@ -2,50 +2,110 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    inputTypeOptions: [
-      {
-        label: 'Fractional Number',
-        value: 'fractionalNumberInput',
-      },
-      {
-        label: 'Complete Number',
-        value: 'completeNumberInput',
-      },
-      {
-        label: 'Text',
-        value: 'textInput',
-      },
-      {
-        label: 'Select (One)',
-        value: 'selectOne',
-      },
-      {
-        label: 'Select (Multiple)',
-        value: 'selectMultiple'
-      }
-    ],
+    categories: [],
+    inputTypeOptions: [{
+      label: 'Fractional Number',
+      value: 'fractionalNumber',
+    },
+    {
+      label: 'Complete Number',
+      value: 'completeNumber',
+    },
+    {
+      label: 'Text (One Line)',
+      value: 'textOneline',
+    },
+    {
+      label: 'Text (Multiline Line)',
+      value: 'textMultiline',
+    },
+    {
+      label: 'Select (One)',
+      value: 'selectOne',
+    },
+    {
+      label: 'Select (Multiple)',
+      value: 'selectMultiple'
+    }],
+
+    filterTypes: [{
+      label: 'Less Then or Equals',
+      value: 'lte'
+    }, {
+      label: 'Less Then',
+      value: 'lt'
+    }, {
+      label: 'Equals',
+      value: 'eq'
+    }, {
+      label: 'Greater Then',
+      value: 'gt'
+    }, {
+        label: 'Greater Then or Equals',
+        value: 'gte'
+    }],
+
+    // Category object which will be sent to the backend
     category: {
-      name: '',
-      description: '',
-      seoDescription: '',
+      categoryId: '',
+      name: 'Mobile',
+      description: 'Category description goes here',
+      seoDescription: 'Subcategory description goes here',
+      isLeaf: true,
       properties: [{
         name: 'Storage',
         required: true,
+        filterable: true,
+        hasUnits: true,
+        units: [{
+          label: 'MB',
+          printLabel: 'MB',
+          threshold: 1024,
+          nextLabel: 'GB'
+        }, {
+          label: 'GB',
+          printLabel: 'GB',
+          threshold: 1024,
+          nextLabel: 'TB'
+        }, {
+          label: 'TB',
+          printLabel: 'TB',
+          threshold: 1024,
+          nextLabel: 'PB'
+        }, {
+          label: 'PB',
+          printLabel: 'PB'
+        }],
+        filterChoices: [],
         input: {
-          type: '',
-          propertyChoices: [{
-            name: 'Android',
-            value: 'android'
-          }]
+          type: 'fractionalNumber',
+          propertyChoices: []
         }
-      },{
+      }, {
         name: 'RAM',
         required: true,
+        filterable: true,
         input: {
-          type: '',
+          type: 'fractionalNumber',
+          propertyChoices: []
+        },
+        units: [],
+        filterChoices: []
+      }, {
+        name: 'OS',
+        required: true,
+        filterable: true,
+        input: {
+          type: 'selectOne',
           propertyChoices: [{
-            label: '',
-            value: ''
+            label: 'Android',
+            value: 'android'
+          }, {
+            label: 'iOS',
+            value: 'ios'
+          }, {
+            label: 'Windows',
+            value: 'windows'
           }]
         }
       }]
@@ -57,33 +117,75 @@ var app = new Vue({
       console.log(this.category);
     },
 
+    // Property methods
     addNewProperty: function() {
       this.category.properties.push({
-        name: 'Storage',
+        name: '',
         required: true,
+        filterChoices: [],
+        units: [],
         input: {
-          type: '',
-          propertyChoices: [{
-            name: 'Android',
-            value: 'android'
-          }]
+          type: 'fractionalNumber',
+          propertyChoices: []
         }
-      })
+      });
     },
 
     removeProperty: function(index) {
       this.category.properties.splice(index, 1);
     },
 
-    addNewChoice: function(propertyChoices) {
-      propertyChoices.push({
+    // Input Choices methods
+    addNewChoice: function(property) {
+      const choiceObject = {
         name: '',
         value: ''
-      })
+      };
+
+      if (Array.isArray(property.input.propertyChoices)) {
+        return property.input.propertyChoices.push(choiceObject);
+      }
+      property.input.propertyChoices = [choiceObject];
     },
 
-    removeChoice: function(index, choices) {
-      choices.splice(index, 1);
-    }
+    removeChoice: function(index, property) {
+      property.input.propertyChoices.splice(index, 1);
+    },
+
+    // Units
+    addUnit(property) {
+      const unitObject = {
+          label: '',
+          printLabel: '',
+          threshold: 10,
+          nextLabel: ''
+      };
+
+      if (Array.isArray(property.units)) {
+        return property.units.push(unitObject);
+      }
+      property.units = [unitObject];
+    },
+
+    removeUnit: function(index, units) {
+      units.splice(index, 1);
+    },
+
+    // Filters
+    addFilter(property) {
+      const filterObject = {
+        label: '',
+        printLabel: '',
+        type: ''
+      };
+      if (Array.isArray(property.filterChoices)) {
+        return property.filterChoices.push(filterObject);
+      }
+      property.filterChoices = [filterObject];
+    },
+
+    removeFilter: function(index, property) {
+      property.filterChoices.splice(index, 1);
+    },
   }
 });
