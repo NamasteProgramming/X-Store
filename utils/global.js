0b7660e39d76d45c73ca5bcfdc37abc18c21dfe7
Namaste Programming <namasteprogramming@gmail.com>
2020-07-24 23:07:44 +0530

    Day 35 - Santize user input

--- utils/global.js
+++ utils/global.js
@@ -15,6 +15,10 @@ const trimObject = (obj) => {
   }
 }
 
+/**
+ * Trim string values and converts emails to lowercase
+ * @param {Object} obj Object which is being sanitized
+ */
 const santizeObject = (obj) => {
   for (const key in obj) {
     if (isString(obj[key])) {
@@ -35,8 +39,37 @@ const santizeObject = (obj) => {
   }
 }
 
+/**
+ * Deletes falsy strings, trims strings and changes emails to lowercase
+ * @param {Object} obj Object which is being trimmed and sanitized
+ */
+const trimAndsantizeObject = (obj) => {
+  for (const key in obj) {
+    if (isString(obj[key])) {
+      if (!obj[key].trim()) {
+        delete obj[key]
+        continue
+      }
+      if (key.indexOf('password') !== -1) {
+        continue
+      }
+      obj[key] = obj[key].trim()
+      if (key === 'email') {
+        obj[key] = obj[key].toLowerCase()
+      }
+    } else if (Array.isArray(obj[key])) {
+      for (let i = 0; i < obj[key].length; i++) {
+        santizeObject(obj[key][i])
+      }
+    } else if (isObject(obj[key])) {
+      santizeObject(obj[key])
+    }
+  }
+}
+
 module.exports = {
   trimObject,
   santizeObject,
+  trimAndsantizeObject,
   isString
 }
